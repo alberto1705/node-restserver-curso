9047da1fcbf72ef8f3ab0546e4abae2447a25945
Alberto Madrid <alberto.madrid@ext.aqualogybs.net>
2019-12-09 15:30:18 +0100

    Rest Server -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,16 @@
 //PUERTO
 process.env.PORT = process.env.PORT || 3000;
+
+//ENTORNO
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//BD
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://beto:SLjS8JBXE1ezzkh9@cluster0-lxune.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -15,9 +15,10 @@ app.use(bodyParser.json())
 
 app.use(require('./routes/usuario'));
 
-mongoose.connect('mongodb://localhost:27017/cafe', {
+mongoose.connect(process.env.URLDB, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
 }, (err, res) => {
     if (err) throw err;
     console.log('Base de datos ONLINE');
